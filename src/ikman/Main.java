3f49436e33772b566fae880f4c9f5138e10d9c8a
prasangafdz@outlook.com <LykanHypersp0rt@#>
2018-11-17 20:46:00 +0530

    Added Selenium.

--- src/ikman/Main.java
+++ src/ikman/Main.java
@@ -1,11 +1,15 @@
 package ikman;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// Ikman.lk assignment
 		//Created by "Prasanga Fernando" 
 		
+		WebDriver driver = new ChromeDriver();
 		
 
 	}
